de5a8fafd9cab08f1aa03e2f4fca35543c12145d
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-27 11:52:46 +0200

    display added zoo animals

--- src/Components/CurrentZoo/MyAnimals.js
+++ src/Components/CurrentZoo/MyAnimals.js
@@ -2,12 +2,32 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class MyAnimals extends Component {
+  displayCurrentZooAnimals = () => {
+    return this.props.currentZoo.animals.map((animal, i) => {
+      return (
+        <div key={i}>
+          <div>
+            {animal.name}
+            {animal.price}
+            <img
+              className="animalImage"
+              src={`assets/images/Animals/${animal.imageName}`}
+              alt="animal"
+            />
+          </div>
+        </div>
+      );
+    });
+  };
   render() {
-    return <div>hi</div>;
+    return <div>j{this.displayCurrentZooAnimals()}</div>;
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => {
+  const { currentZoo } = state;
+  return { currentZoo };
+};
 
 const mapDispatchToProps = {};
 
