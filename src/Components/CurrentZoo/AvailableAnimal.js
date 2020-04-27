7625f730a506764b6a755ad1877c37af48b23751
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-27 10:42:51 +0200

    displayed all the available animals and costs

--- src/Components/CurrentZoo/AvailableAnimal.js
+++ src/Components/CurrentZoo/AvailableAnimal.js
@@ -2,11 +2,50 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class AvailableAnimal extends Component {
-  render() {
+  displayImage = () => {
+    //change to tenary
+    if (this.props.animal.imageName === undefined) {
+      return (
+        <img
+          className="animalImage"
+          src={"assets/images/Animals/Placeholder.png"}
+          alt="noAnimal"
+        />
+      );
+    } else {
+      return (
+        <img
+          className="animalImage"
+          src={`assets/images/Animals/${this.props.animal.imageName}`}
+          alt="animal"
+        />
+      );
+    }
+  };
+
+  displayCosts = () => {
+    let price = this.props.animal.price;
     return (
       <div>
-        {this.props.animal.name}
-        {this.props.animal.price}
+        <img
+          className="coins"
+          src={`assets/images/Coins/2coins.svg`}
+          alt="coins"
+        />
+        {price}
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div className="displayAnimalDetails">
+        <div className="animalNamePhoto">
+          {this.props.animal.name}
+          {this.displayImage()}
+        </div>
+        <div>{this.displayCosts()}</div>
+        <button> + add</button>
       </div>
     );
   }
