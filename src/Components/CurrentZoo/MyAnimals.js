f8b2d6f4c6a57ed53b1937d957b04456ce38eddb
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-27 12:13:21 +0200

    added styling my animals page

--- src/Components/CurrentZoo/MyAnimals.js
+++ src/Components/CurrentZoo/MyAnimals.js
@@ -5,12 +5,18 @@ export class MyAnimals extends Component {
   displayCurrentZooAnimals = () => {
     return this.props.currentZoo.animals.map((animal, i) => {
       return (
-        <div key={i}>
-          <div>
-            {animal.name}
-            {animal.price}
+        <div key={i} className="singleAnimalContainer">
+          <div className="animalPrice">
             <img
-              className="animalImage"
+              className="coins"
+              src={`assets/images/Coins/2coins.svg`}
+              alt="coins"
+            />
+            {animal.price}
+          </div>
+          <div>
+            <img
+              className="singleAnimalContainerAnimalImage"
               src={`assets/images/Animals/${animal.imageName}`}
               alt="animal"
             />
@@ -20,7 +26,12 @@ export class MyAnimals extends Component {
     });
   };
   render() {
-    return <div>j{this.displayCurrentZooAnimals()}</div>;
+    return (
+      <div>
+        My Animals
+        <div className="myAnimals">{this.displayCurrentZooAnimals()}</div>
+      </div>
+    );
   }
 }
 
